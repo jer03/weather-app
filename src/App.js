2004b13a131b4d16bae569847ec0283cded23dd0
jer03 <jermcardenas@gmail.com>
2024-05-29 04:58:20 -0600

    more changes, modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Weather from './weather.jsx'
+import Weather from './components/getweather.jsx'
 
 function App() {
   
   return (
     <div className="App">
+      <h1> The Simple Weather App</h1>
+      <img className="images" src="/cloud.png" alt="clouds" />
       <Weather />
     </div>
   );
---
+++ src/components/getweather.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial`);
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <>
+      <div id="container" >
+        <div id="text"> Enter City Here:</div>
+        <input id="input" type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter le city"/>
+        <button id="button" onClick={getWeather}><FontAwesomeIcon icon={faSearch} /></button> 
+      </div>
+      {weatherData && (
+        <div id="content">
+          <h1>{weatherData.name}, {weatherData.sys.country}</h1>
+          <h2>Temperature: {weatherData.main.temp} °F</h2>
+          <h2>Weather: {weatherData.weather[0].main}</h2>
+          <h2>Feels like: {weatherData.main.feels_like} °F</h2>
+        </div>
+      )}
+    </>
+    
+  );
+}
+
+export default Weather;
